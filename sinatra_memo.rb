77bb620d69a63e27bd0071972b20e5e2e35cbe1a
haramika <genemika0515@icloud.com>
2025-10-05 18:36:24 +0900

    post,patch,deleteの実装を追加

--- sinatra_memo.rb
+++ sinatra_memo.rb
@@ -2,23 +2,80 @@
 
 require 'sinatra'
 require 'sinatra/reloader'
+require 'json'
 
 get '/memo/top' do
-  erb :top_page
+  json_date = File.open('memo.json') do |file|
+    JSON.parse(file.read)
+  end
+  json_dates = json_date['memos']
+
+  @json_date = json_dates
+  erb :top
 end
 
-get '/memo/new_memo' do
-  erb :new_memo
+post '/memo/top' do
+  title = params[:title]
+  body = params[:content]
+
+  existing_date = JSON.parse(File.read('memo.json'))
+  existing_date['memos'].push({ 'id' => existing_date['memos'].size + 1, 'title' => title, 'body' => body })
+
+  File.open('memo.json', 'w') do |f|
+    JSON.dump(existing_date, f)
+  end
+
+  redirect '/memo/top'
 end
 
-get '/memo/show_memo1' do
-  @memo_title = "メモ１"
-  @content = "内容１"
-  erb :show_memo1
+patch '/memo/top' do
+  title = params[:title]
+  body = params[:content]
+
+  existing_date = JSON.parse(File.read('memo.json'))
+  existing_date['memos'][params[:id].to_i - 1]['title'] = title
+  existing_date['memos'][params[:id].to_i - 1]['body'] = body
+
+  File.open('memo.json', 'w') do |f|
+    JSON.dump(existing_date, f)
+  end
+
+  redirect '/memo/top'
 end
 
-get '/memo/edit_memo1' do
-  @memo_title = "メモ１"
-  @content = "内容１"
-  erb :edit_memo1
+delete '/memo/top' do
+  existing_date = JSON.parse(File.read('memo.json'))
+  existing_date['memos'].delete_at(params[:id].to_i - 1)
+
+  File.open('memo.json', 'w') do |f|
+    JSON.dump(existing_date, f)
+  end
+
+  redirect '/memo/top'
+end
+
+get '/memo/new' do
+  erb :new
+end
+
+get '/memo/:id' do
+  json_date = File.open('memo.json') do |file|
+    JSON.parse(file.read)
+  end
+  json_dates = json_date['memos']
+
+  @json_date = json_dates
+  @id = params[:id].to_i - 1
+  erb :show
+end
+
+get '/memo/:id/edit' do
+  json_date = File.open('memo.json') do |file|
+    JSON.parse(file.read)
+  end
+  json_dates = json_date['memos']
+
+  @json_date = json_dates
+  @id = params[:id].to_i - 1
+  erb :edit
 end
