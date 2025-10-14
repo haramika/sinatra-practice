7fca177ac4f68164278584f2cc6863bea9a8433e
haramika <genemika0515@icloud.com>
2025-10-14 10:29:33 +0900

    dateをdataに修正

--- sinatra_memo.rb
+++ sinatra_memo.rb
@@ -4,13 +4,24 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'json'
 
-get '/memo/top' do
-  json_date = File.open('memo.json') do |file|
+def load_json
+  File.open('memo.json') do |file|
     JSON.parse(file.read)
   end
-  json_dates = json_date['memos']
+end
 
-  @json_date = json_dates
+def read_json
+  JSON.parse(File.read('memo.json'))
+end
+
+def open_json(data)
+  File.open('memo.json', 'w') do |f|
+    JSON.dump(data, f)
+  end
+end
+
+get '/memo/top' do
+  @json_data = load_json['memos']
   erb :top
 end
 
@@ -18,12 +29,10 @@ post '/memo/top' do
   title = params[:title]
   body = params[:content]
 
-  existing_date = JSON.parse(File.read('memo.json'))
-  existing_date['memos'].push({ 'id' => existing_date['memos'].size + 1, 'title' => title, 'body' => body })
+  existing_data = JSON.parse(File.read('memo.json'))
+  existing_data['memos'].push({ 'id' => existing_data['memos'].size + 1, 'title' => title, 'body' => body })
 
-  File.open('memo.json', 'w') do |f|
-    JSON.dump(existing_date, f)
-  end
+  open_json(existing_data)
 
   redirect '/memo/top'
 end
@@ -32,24 +41,20 @@ patch '/memo/top' do
   title = params[:title]
   body = params[:content]
 
-  existing_date = JSON.parse(File.read('memo.json'))
-  existing_date['memos'][params[:id].to_i - 1]['title'] = title
-  existing_date['memos'][params[:id].to_i - 1]['body'] = body
+  existing_data = JSON.parse(File.read('memo.json'))
+  existing_data['memos'][params[:id].to_i - 1]['title'] = title
+  existing_data['memos'][params[:id].to_i - 1]['body'] = body
 
-  File.open('memo.json', 'w') do |f|
-    JSON.dump(existing_date, f)
-  end
+  open_json(existing_data)
 
   redirect '/memo/top'
 end
 
 delete '/memo/top' do
-  existing_date = JSON.parse(File.read('memo.json'))
-  existing_date['memos'].delete_at(params[:id].to_i - 1)
+  existing_data = JSON.parse(File.read('memo.json'))
+  existing_data['memos'].delete_at(params[:id].to_i - 1)
 
-  File.open('memo.json', 'w') do |f|
-    JSON.dump(existing_date, f)
-  end
+  open_json(existing_data)
 
   redirect '/memo/top'
 end
@@ -59,23 +64,13 @@ get '/memo/new' do
 end
 
 get '/memo/:id' do
-  json_date = File.open('memo.json') do |file|
-    JSON.parse(file.read)
-  end
-  json_dates = json_date['memos']
-
-  @json_date = json_dates
+  @json_data = load_json['memos']
   @id = params[:id].to_i - 1
   erb :show
 end
 
 get '/memo/:id/edit' do
-  json_date = File.open('memo.json') do |file|
-    JSON.parse(file.read)
-  end
-  json_dates = json_date['memos']
-
-  @json_date = json_dates
+  @json_data = load_json['memos']
   @id = params[:id].to_i - 1
   erb :edit
 end
